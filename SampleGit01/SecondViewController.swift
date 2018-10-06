20fcdd09be559cda3c75d687d40f48cfa67aa61c
torkia <42777204+torkia@users.noreply.github.com>
2018-10-06 18:47:42 +0900

    viewの背景色をgreenに変更しました。

--- SampleGit01/SecondViewController.swift
+++ SampleGit01/SecondViewController.swift
@@ -16,7 +16,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.yellow
+        self.view.backgroundColor = UIColor.green
         
         label1.backgroundColor = UIColor.yellow
         label1.text = "ラベル1"
